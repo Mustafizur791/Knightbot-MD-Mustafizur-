0ec13f8f5619c4bf7171dea463ff77e5106b0593
Mustafizur791 <mustafizurbot0@gmail.com>
2025-04-16 04:00:35 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'Mustafizur ', // Your name
+  ownerNumber: '+60195168673', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
